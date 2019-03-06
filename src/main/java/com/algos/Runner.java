87ab403217ce44573c602a2ffd2fa79971e13286
Xavier <toxavier@lm-blr-16500020.corp.ebay.com>
2019-03-06 12:15:21 +0530

    make runnable

--- src/main/java/com/algos/Runner.java
+++ src/main/java/com/algos/Runner.java
@@ -2,9 +2,9 @@ package com.algos;
 
 public class Runner {
 	
-	public static void main(String args)
+	public static void main(String args[])
 	{
-		
+		System.out.println("Listing...on Mac");
 		
 	}
 
